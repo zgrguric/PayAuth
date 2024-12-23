6b47ae5e990241b741f26c905613ad4319b82211
Lathan <jucallme@gmail.com>
2024-12-23 12:44:01 -0400

    prep for scan

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -35,7 +35,7 @@
     </div>
 
    
-    <footer  v-if="isLoading == false" class="container bg-black footer position-absolute bottom-0 start-50 translate-middle-x text-center">
+    <footer  v-if="!isLoading" class="container bg-black footer position-absolute bottom-0 start-50 translate-middle-x text-center">
         <span class="text-light fancy-font position-absolute bottom-0 start-0 ms-2 mb-4">scan qr code -> </span>
         <button @click="openScan" class="btn btn-default mt-2 mb-4" role="button" id="open-sign">
             <img src="/scan-touch-icon.png" class="border border-1 rounded-3" alt="open sign" width="55" />
@@ -67,6 +67,7 @@
                 await this.getAccountObjects()
                 await this.xAppListeners()
             }
+            this.isLoading = false
         },
         computed: {
             ledger() {
