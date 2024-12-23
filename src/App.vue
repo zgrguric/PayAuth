151976e938365e5f0b115e8a88e107d8c8cb7e0c
Lathan <jucallme@gmail.com>
2024-12-23 12:52:15 -0400

    styling

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <footer  v-if="!isLoading" class="container bg-black footer fixed-bottom start-50 translate-middle-x text-center">
         <span class="text-light fancy-font position-absolute bottom-0 start-0 ms-2 mb-4">scan qr code -> </span>
         <button @click="openScan" class="btn btn-default mt-2 mb-4" role="button" id="open-sign">
-            <img src="/scan-touch-icon.png" class="border border-1 rounded-3" alt="open sign" width="55" />
+            <img src="/scan-touch-icon.png" class="border border-0 rounded-3" alt="open sign" width="55" />
         </button>
     </footer>
 </template>
