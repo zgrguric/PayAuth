5acff424b2a96be72f512eae24307dc75c58622c
Lathan <jucallme@gmail.com>
2025-01-06 19:36:56 -0400

    i18n test

---
+++ src/i18n.js
@@ -0,0 +1,20 @@
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import es from './locales/es.json'
+
+function loadLocaleMessages() {
+    const locales = [{ en: en }, { ar: ar }]
+    const messages = {}
+    locales.forEach((lang) => {
+        const key = Object.keys(lang)
+        messages[key] = lang[key]
+    })
+    return messages
+}
+
+export default createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    legacy: false,
+    messages: loadLocaleMessages(),
+})
