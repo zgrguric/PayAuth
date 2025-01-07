b24dc74b4af5fee3945c2874855ed85148140f4c
Lathan <jucallme@gmail.com>
2025-01-06 20:04:17 -0400

    i18n test

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import mkcert from 'vite-plugin-mkcert'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
 export default defineConfig({
-  plugins: [mkcert(), vue(), 
-    vueI18n({
+  plugins: [mkcert(), vue(), vueI18n({
       include: path.resolve(__dirname, './src/locales/**')
-    })
-  ],
+    })],
   server: {
     https: true,
     port: 5500,
