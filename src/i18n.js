def21bd3a33273c42cd095e265dc5c9105cee93a
Lathan <jucallme@gmail.com>
2025-01-06 21:22:59 -0400

    wip translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,11 @@
 import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import es from './locales/es.json'
+import ja from './locales/ja.json'
+import af from './locales/af.json'
 
 function loadLocaleMessages() {
-    const locales = [{ en: en }, { es: es }]
+    const locales = [{ en: en }, { es: es }, { ja: ja }, { af: af }]
     const messages = {}
     locales.forEach((lang) => {
         const key = Object.keys(lang)
