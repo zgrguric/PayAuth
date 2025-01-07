1033f1555ebd3d2e1caab389636fed5c8a7f633a
Lathan <jucallme@gmail.com>
2025-01-07 12:25:56 -0400

    add support of new languages

--- src/i18n.js
+++ src/i18n.js
@@ -3,9 +3,11 @@ import en from './locales/en.json'
 import es from './locales/es.json'
 import ja from './locales/ja.json'
 import af from './locales/af.json'
+import hr from './locales/hr.json'
+import ca from './locales/ca.json'
 
 function loadLocaleMessages() {
-    const locales = [{ en: en }, { es: es }, { ja: ja }, { af: af }]
+    const locales = [{ en: en }, { es: es }, { ja: ja }, { af: af }, { hr: hr } , { ca: ca }]
     const messages = {}
     locales.forEach((lang) => {
         const key = Object.keys(lang)
